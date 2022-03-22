5a3ceeccf992fe1eea39e24c33998daa231ed343
evil3sc <hyp3rbone@outlook.com>
2022-03-21 22:09:02 -0600

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,147 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import { HeadBar } from './components/HeadBar'
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+import TextField from '@mui/material/TextField';
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 function App() {
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  
+  const handleChange = (event) => {
+    setRolClass(event.target.value);
+  };
+
+  const [rolClass, setRolClass] = React.useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeadBar />
+      <br />
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item sm={8} xs={12}>
+            <Box
+              component="form"
+              sx={{
+                '& > :not(style)': { m: 0, width: '100%' },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField id="standard-basic" label="Nombre" variant="standard" />
+            </Box>
+            <FormControl variant="standard" sx={{ mt: '1ch', minWidth: '100%' }}>
+              <InputLabel id="rol-class-selection">Clase</InputLabel>
+              <Select
+                labelId="class-selection"
+                id="class-selection"
+                value={rolClass}
+                onChange={handleChange}
+                label="Class"
+              >
+                <MenuItem value="">
+                  <em>Ninguna</em>
+                </MenuItem>
+                <MenuItem value={10}>Guerrero</MenuItem>
+                <MenuItem value={20}>Samurai</MenuItem>
+                <MenuItem value={30}>Asesino</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item sm={4} xs={12}>
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={0}>
+                <Grid item xs={12}>
+                  <Box sx={{ display: 'flex', alignItems: 'flex-center', width: '100%'}}>
+                    <TextField
+                      id="pv-number"
+                      label="PV"
+                      type="number"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      variant="standard"
+                    />
+                    <Box sx={{marginLeft: '1ch', marginRight: '1ch'}}>
+                      <p>/</p>
+                    </Box>
+                    <TextField 
+                      id="total-pv-input" 
+                      label=" "
+                      defaultValue="99"
+                      type="number"
+                      variant="standard" 
+                      InputProps={{ 
+                        readOnly: true,
+                      }}
+                    />
+                  </Box>
+                </Grid>
+                <Grid item xs={12}>
+                  <Box sx={{ display: 'flex', alignItems: 'flex-center', width: '100%'}}>
+                    <TextField 
+                      id="lvl-input" 
+                      label="LVL"
+                      type="number"
+                      defaultValue="1"
+                      variant="standard" 
+                      InputProps={{ 
+                        readOnly: true,
+                      }}
+                    />
+                    <Box sx={{marginLeft: '2ch', marginRight: '1ch'}}>
+                    </Box>
+                    <TextField
+                      id="xp-number"
+                      label="XP"
+                      type="number"
+                      defaultValue="0"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      variant="standard"
+                    />
+                  </Box>
+                </Grid>
+              </Grid>
+            </Box>
+          </Grid>
+          <Grid item sm={2} xs={6}>
+            //Atributos A
+            
+          </Grid>
+          <Grid item sm={2} xs={6}>
+            <Item>atribs b</Item>
+          </Grid>
+          <Grid item sm={8} xs={12}>
+            <Item>skills</Item>
+          </Grid>
+          <Grid item sm={6} xs={12}>
+            <Item>equip</Item>
+          </Grid>
+          <Grid item sm={6} xs={12}>
+            <Item>inv</Item>
+          </Grid>
+        </Grid>
+      </Container>
+      
+    </>
   );
 }
 
---
+++ src/components/HeadBar.jsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+
+export function HeadBar() {
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+      });
+    
+    const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+    }
+
+    setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+    <Box
+        sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+    >
+        <List>
+            <ListItem button key='char'>
+                <ListItemIcon>
+                    <InboxIcon />  
+                </ListItemIcon>
+                <ListItemText primary='Character' />
+            </ListItem>
+            <ListItem button key='newchar'>
+                <ListItemIcon>
+                    <InboxIcon />  
+                </ListItemIcon>
+                <ListItemText primary='New Character' />
+            </ListItem>
+            <ListItem button key='system'>
+                <ListItemIcon>
+                    <InboxIcon />  
+                </ListItemIcon>
+                <ListItemText primary='Rol System' />
+            </ListItem>
+        </List>
+        <Divider />
+    </Box>
+    );
+    
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleDrawer('left', true)}>
+                    <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit" component="div">
+                    Character Sheet
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                anchor={'left'}
+                open={state['left']}
+                onClose={toggleDrawer('left', false)}
+            >
+                {list('left')}
+            </Drawer>
+        </>
+    );
+}
